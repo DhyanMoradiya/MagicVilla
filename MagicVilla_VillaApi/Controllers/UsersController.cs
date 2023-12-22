11182e97290c4fc3f01d66b2eebee415d603521b
Dhyan <mrkingmoradiya@gmail,com>
2023-12-22 18:44:54 +0530

    Created fuctional User API endpoint

---
+++ MagicVilla_VillaApi/Controllers/UsersController.cs
@@ -0,0 +1,73 @@
+﻿using MagicVilla_VillaApi.Model;
+using MagicVilla_VillaApi.Model.Dto;
+using MagicVilla_VillaApi.Repository.IRepository;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using System.Net;
+using System.Text.RegularExpressions;
+
+namespace MagicVilla_VillaApi.Controllers
+{
+    [Route("api/UsersAuth")]
+    [ApiController]
+    public class UsersController : ControllerBase
+    {
+        private readonly IUserRepository _userRepo;
+        private APIResponse _response;
+
+        public UsersController(IUserRepository userRepo)
+        {
+            _userRepo = userRepo;
+            _response = new APIResponse();  
+        }
+
+        [HttpPost("login")]
+        [ProducesResponseType(StatusCodes.Status200OK)]
+        [ProducesResponseType(StatusCodes.Status400BadRequest)]
+        public async Task<IActionResult> Login(LoginRequestDTO loginRequestDTO)
+        {
+            if(loginRequestDTO == null)
+            {
+                _response.StatusCode = HttpStatusCode.BadRequest;
+                _response.IsSuccess = false;
+                _response.ErrorMessages.Add("Data not provided");
+                return BadRequest(_response);
+            }
+            LoginResponseDTO loginResponseDTO = await _userRepo.Login(loginRequestDTO);
+            if(loginResponseDTO.User == null || String.IsNullOrEmpty(loginResponseDTO.Token)) {
+                _response.StatusCode = HttpStatusCode.BadRequest;
+                _response.IsSuccess = false;
+                _response.ErrorMessages.Add("Username or Password is invalid");
+                return BadRequest(_response);
+            }
+            _response.StatusCode = HttpStatusCode.OK;
+            _response.Result = loginResponseDTO;
+            return Ok(_response);
+        }
+
+        [HttpPost]
+        [ProducesResponseType(StatusCodes.Status200OK)]
+        [ProducesResponseType(StatusCodes.Status400BadRequest)]
+        public async Task<IActionResult> Register(RegisterationRequestDTO registerationRequestDTO)
+        {
+            bool isUniqueUsername = await _userRepo.IsUniqueUser(registerationRequestDTO.UserName);
+            if(!isUniqueUsername)
+            {
+                _response.StatusCode = HttpStatusCode.BadRequest;
+                _response.IsSuccess = false;
+                _response.ErrorMessages.Add("Username already Exists");
+                return BadRequest(_response);
+            }
+           LocalUser user =  await _userRepo.Register(registerationRequestDTO);
+            if(user == null)
+            {
+                _response.StatusCode = HttpStatusCode.BadRequest;
+                _response.IsSuccess = false;
+                _response.ErrorMessages.Add("Error While registoring");
+                return BadRequest(_response);
+            }
+            _response.StatusCode = HttpStatusCode.OK;
+            return Ok(_response);
+        }
+    }
+}
--- MagicVilla_VillaApi/Program.cs
+++ MagicVilla_VillaApi/Program.cs
@@ -17,6 +17,7 @@ builder.Services.AddDbContext<ApplicationDbContext>(options =>
 builder.Services.AddAutoMapper(typeof(MappingConfig));
 builder.Services.AddScoped<IVillaRepository, VillaRepository>();
 builder.Services.AddScoped<IVillaNumberRepository, VillaNumberRepository>();
+builder.Services.AddScoped<IUserRepository, UserRepository>();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
